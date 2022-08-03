a64182fe89071234ad4af0998c4af8fef05a8f5d
Pratik <pratik.panda119@gmail.com>
2022-08-03 20:36:55 +0530

    add date.js

---
+++ date.js
@@ -0,0 +1,12 @@
+function getDate() {
+    let date = new Date();
+    let options = {
+        weekday : 'long',
+        day : 'numeric',
+        month : 'long'
+    }
+    let today = date.toLocaleDateString('en-US', options);
+    return today;
+}
+
+module.exports = getDate;
