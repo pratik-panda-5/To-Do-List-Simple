1b09fc066c3a9ca2886c389fcdf717ebd19dce26
Pratik <pratik.panda119@gmail.com>
2022-08-03 19:39:26 +0530

    make final

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
-const { func } = require('assert-plus');
-
 const app = express();
+
+
+let items = []; 
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res)
 {
-    res.send("Hello!");
+    let date = new Date();
+    let options = {
+        weekday : 'long',
+        day : 'numeric',
+        month : 'long'
+    }
+    let today = date.toLocaleDateString('en-US', options);
+    res.render('list', {today : today, items : items});
 });
 
+app.get('/about', function(req, res)
+{
+    res.render('about');
+});
+
+app.post('/', function (req, res) {
+    let thing = req.body.newItem;
+    thing = thing.trim();
+    if(thing != "")
+    {
+        items.push(thing);
+    }
+    res.redirect('/');
+});
 
 app.listen(3000, function () {      
     console.log("server started!");
