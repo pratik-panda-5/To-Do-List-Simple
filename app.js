59bd7e8df0eb79abda28718021640cab7509a274
Pratik <pratik.panda119@gmail.com>
2022-08-18 12:47:25 +0530

    integrated mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const date = require(__dirname + "/date.js");
 
-console.log(date);
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+
+const itemSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    }
+});
+const Item = mongoose.model("Item", itemSchema);
+
+
 
 let items = []; 
 
@@ -13,7 +24,37 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res)
 {
-    res.render('list', {today : date(), items : items});
+    Item.find({}, (err, items)=>{
+        if(items.length === 0)
+        {
+            const item1 = new Item({
+                name : 'Welcome to to-do list'
+            });
+            const item2 = new Item({
+                name : 'Click on + button to add new tasks'
+            });
+            const item3 = new Item({
+                name : '<- Press this to delete task'
+            });
+            const defaultItems = [item1, item2, item3];
+            
+            Item.insertMany(defaultItems, (err)=>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("default items inserted");
+                    // Item.find({}, (err, items)=>{
+                    //     res.render('list', {today : date(), items : items});
+                    // });
+                }
+            });
+            res.redirect("/");
+        }
+        else{
+            res.render('list', {today : date(), items : items});
+        }
+    });
 });
 
 app.get('/about', function(req, res)
@@ -26,9 +67,26 @@ app.post('/', function (req, res) {
     thing = thing.trim();
     if(thing != "")
     {
-        items.push(thing);
+        const item = new Item({
+            name : thing
+        });
+        item.save();
     }
     res.redirect('/');
+});
+
+app.post('/delete', (req, res)=>{
+    Item.deleteOne({_id : req.body.checkbox}, (err)=>{
+        if(err)
+        {
+            console.log("vsad");
+        }
+        else{
+            setTimeout(() => {
+                res.redirect('/');
+            }, 500);
+        }
+    })
 });
 
 app.listen(3000, function () {      
